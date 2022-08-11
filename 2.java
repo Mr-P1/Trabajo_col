9bf183481eaefbaa18eb359866012c11d9341a3a
Mr-P1 <jo.salvo@duocuc.cl>
2022-08-11 10:21:39 -0400

    Agrege 2.java y elimine miArchivo

---
+++ 2.java
@@ -0,0 +1,5 @@
+public main void{
+
+    public number telefono;
+
+}
--- archivo3.py
+++ archivo3.py
@@ -1,5 +1,11 @@
 import os
+os.("cls")
 
+u=0
 while True:
     print("hola")
-    
+    u += 1
+
+    if u = 4 :
+        break
+        
